7c80f593c49243cfafd436123a1dc2fb565f7102
Steve Wang <steve.wang@prendi.com.au>
2020-08-06 18:13:39 +1000

    update settings, add install scripts, add zsh plugin

---
+++ install_scripts.sh
@@ -0,0 +1,12 @@
+# install nvm and node
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.zshrc
+nvm install 10
+
+npm install -g typescript
+npm install -g ts-node
+
+sudo apt install htop
+
+# install aws, ngrok, workbench
+
---
+++ sh/install_zsh.sh
@@ -0,0 +1,20 @@
+# install zsh
+# cat /etc/shells
+apt install zsh -y
+chsh -s /bin/zsh
+# reboot
+sudo install python git -y
+
+
+# install oh my zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+# p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# on-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM1:-$ZSH/custom}/plugins/alias-tips
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+source ~/.zshrc
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
