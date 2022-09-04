32b03175421937c409ece5764d805073570e090e
Steve Wang <wangqi.1992@hotmail.com>
2022-09-04 19:17:47 +1000

    update install script and zsh profile

--- install_scripts.sh
+++ install_scripts.sh
@@ -39,6 +39,11 @@ curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbun
 sudo apt update
 sudo apt install albert -y
 
+# peek
+sudo add-apt-repository ppa:peek-developers/stable
+sudo apt update
+sudo apt install peek
+
 # spotify
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -53,3 +58,10 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 rm -rf ./aws awscliv2.zip
+
+# sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text -y
